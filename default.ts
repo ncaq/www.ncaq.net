855b0ef1e3363a5bf7cc1ae1e6e878e6aeb183e6
ncaq <ncaq@ncaq.net>
2016-10-22 20:17:36 +0900

    added: default.js

---
+++ default.ts
@@ -0,0 +1,21 @@
+interface Window {
+    ga: any;
+}
+
+try {
+    window.ga = window.ga || function() {
+        (window.ga.q = window.ga.q || []).push(arguments);
+    };
+    window.ga.l = +new Date;
+
+    window.ga("create", "UA-82769478-1", "auto");
+    window.ga("send", "pageview");
+} catch (e) {
+    console.error(e);
+}
+
+try {
+
+} catch (e) {
+    console.error(e);
+}
