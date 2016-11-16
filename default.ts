8e989c7930256c02885477cca191c44e70402f9f
ncaq <ncaq@ncaq.net>
2016-11-16 18:21:28 +0900

    added: adsbygoogle

--- default.ts
+++ default.ts
@@ -1,4 +1,5 @@
 interface Window {
+    adsbygoogle: any;
     ga: any;
 }
 
@@ -10,6 +11,16 @@ try {
 
     window.ga("create", "UA-82769478-1", "auto");
     window.ga("send", "pageview");
+} catch (e) {
+    console.error(e);
+}
+
+declare var adsbygoogle: any;
+try {
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-9196585492657164",
+        enable_page_level_ads: true
+    });
 } catch (e) {
     console.error(e);
 }
