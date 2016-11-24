ba0d6c543ff4cbe7aec347157f71d0b2687caac4
ncaq <ncaq@ncaq.net>
2016-11-24 16:40:36 +0900

    changed: use sync google analytics

--- default.ts
+++ default.ts
@@ -1,16 +1,4 @@
 try {
-    ga = ga || function() {
-        (ga.q = ga.q || []).push(arguments);
-    };
-    ga.l = +new Date;
-
-    ga("create", "UA-82769478-1", "auto");
-    ga("send", "pageview");
-} catch (e) {
-    console.error(e);
-}
-
-try {
     const hljs = require("highlight.js");
     hljs.initHighlighting();
 } catch (e) {
