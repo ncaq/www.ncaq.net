4099c3dfad8c3d031111d37a1dd741df32ea5b29
ncaq <ncaq@ncaq.net>
2017-02-10 18:00:44 +0900

    changed: teaserFieldByResource 300 -> 195

--- site.hs
+++ site.hs
@@ -72,7 +72,7 @@ pandocCompilerCustom = pandocCompilerWith
 entryContext :: Context String
 entryContext = mconcat [ cleanUrlField
                        , mconcat entryDate
-                       , teaserFieldByResource 300 "teaser" "content"
+                       , teaserFieldByResource 195 "teaser" "content"
                        , defaultContext]
   where cleanUrlField = field "url" (fmap (maybe empty $ (hyphenToSlash . cleanUrlString) . toUrl) .
                                      getRoute . itemIdentifier)
