70b6be1cf112588df2213c070d8dfb1e4aa20e4e
ncaq <ncaq@ncaq.net>
2017-11-03 18:01:06 +0900

    changed: gtagに移行

--- default.ts
+++ default.ts
@@ -1,16 +1,3 @@
-interface Window
-{
-    ga: any;
-}
-
-try {
-    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-    ga("create", "UA-82769478-1", "auto");
-    ga("send", "pageview");
-} catch (e) {
-    console.error(e);
-}
-
 try {
     const hljs = require("highlight.js");
     hljs.initHighlighting();
