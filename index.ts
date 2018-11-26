7229120a00fa54e3c2170ab8cbb5badc97104c25
ncaq <ncaq@ncaq.net>
2018-11-26 16:16:57 +0900

    changed: ブロッキングscriptを除去

--- index.ts
+++ index.ts
@@ -4,12 +4,10 @@ interface Window {
   adsbygoogle: any;
 }
 
-let adCounter = 0;
-function insertNativeFeedAd(url: string) {
-  adCounter += 1;
-  if (15 < adCounter) {
-    const entry = document.querySelector(`a[href="${url}"]`);
-    if (entry) {
+Array.prototype.forEach.call(
+  document.querySelectorAll(".entry-link"),
+  (entryLink: HTMLElement, index: number) => {
+    if (index !== 0 && index % 15 === 0) {
       const ins = document.createElement("ins");
       ins.setAttribute("class", "adsbygoogle");
       ins.setAttribute("style", "display:block");
@@ -17,9 +15,8 @@ function insertNativeFeedAd(url: string) {
       ins.setAttribute("data-ad-layout-key", "-cn+57+3d-yw+18s");
       ins.setAttribute("data-ad-client", "ca-pub-8393549016688364");
       ins.setAttribute("data-ad-slot", "4040494271");
-      entry.appendChild(ins);
+      entryLink.appendChild(ins);
       (adsbygoogle = window.adsbygoogle || []).push({});
     }
-    adCounter = 0;
   }
-}
+);
