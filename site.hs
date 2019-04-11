88025b8f4cd817a5ae1a41d5bf40e5a18a96dfbe
ncaq <ncaq@ncaq.net>
2019-04-11 22:41:56 +0900

    modified: deploy: verbose -> info=ALL
    `--verbose`は変化のない空のディレクトリも表示して鬱陶しいので.

--- site.hs
+++ site.hs
@@ -60,7 +60,7 @@ main = hakyllWith conf $ do
 
 conf :: Configuration
 conf = def
-  { deployCommand = "rsync --verbose --checksum --archive --chmod=D755,F644 " <>
+  { deployCommand = "rsync --info=ALL --checksum --archive --chmod=D755,F644 " <>
     "--delete --compress --human-readable --progress " <>
     "_site/ ncaq@ncaq.net:/var/www/www.ncaq.net"
   }
