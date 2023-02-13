41c6edf3bf6cb0e79ad2cf2a65ec31f80281b590
ncaq <ncaq@ncaq.net>
2023-02-13 14:53:03 +0900

    refactor: 関数の位置と名前を少し分かり易くする

--- site.hs
+++ site.hs
@@ -15,18 +15,29 @@ import           Text.Regex.TDFA     hiding (empty, match)
 
 main :: IO ()
 main =
-  pre >>=
+  getEntryAndYears >>=
   hakyllRun
 
 -- | 年一覧をスマートにページネーションする方法がわからなかったので`IO`でごり押しする。
 -- `Rules`は内部的には`IO`をベースに持つが、
 -- unsafe系以外で持ち上げる方法が見つからないので、
 -- 別コンテキストで処理する。
-pre :: IO (String, [String])
-pre = do
-  entryIndex <- readFile "entry-index.html"
+getEntryAndYears :: IO (String, [String])
+getEntryAndYears = do
+  entryIndex <- readFile "site/entry-index.html"
   years <- yearInEntry
   return (entryIndex, years)
+
+-- | entryディレクトリ下にある記事が存在する年をリストで返却する。
+-- 0埋めされてる可能性も考慮して`Int`ではなくあえて`String`。
+-- Hakyllのフレームワークに乗りたかった気持ちはあるが、
+-- メタデータではなく`Context`下で取り出す方法がよくわからなかった。
+-- 規則性があるので今回はこれで問題ないと判断した。
+yearInEntry :: IO [String]
+yearInEntry = do
+  entryList <- L.sort <$> listDirectory "site/entry"
+  let getYear = L.takeWhile (/= '-')
+  return $ reverse $ L.nub $ getYear <$> entryList
 
 hakyllRun :: (String, [String]) -> IO ()
 hakyllRun (entryIndex, years) = hakyllWith conf $ do
@@ -253,14 +264,3 @@ tidyXml item = check item >> pure item
           , "--wrap", "0"
           , "-xml"
           ]
-
--- | entryディレクトリ下にある記事が存在する年をリストで返却する。
--- 0埋めされてる可能性も考慮して`Int`ではなくあえて`String`。
--- Hakyllのフレームワークに乗りたかった気持ちはあるが、
--- メタデータではなくContext下で取り出す方法がよくわからなかった。
--- 規則性があるので今回はこれで問題ないと判断した。
-yearInEntry :: IO [String]
-yearInEntry = do
-  entryList <- L.sort <$> listDirectory "entry"
-  let getYear = L.takeWhile (/= '-')
-  return $ reverse $ L.nub $ getYear <$> entryList
