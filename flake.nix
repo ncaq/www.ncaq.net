c37481708343111930127ea87b169b442678d858
ncaq <ncaq@ncaq.net>
2026-03-01 18:11:23 +0900

    build: flake-utilsとhaskellNixをflake-partsに置き換え
    過大な機能のフレームワークから軽量なシステムへの移行。
    
    - flake.nixのinputsからflake-utilsとhaskellNixを削除
    - flake-partsを導入し、outputsの構造をflake-parts方式に変更
    - Haskell, Node.js, Pythonのビルド・環境定義をflake-partsのperSystemで再構成
    - devShells, checks, formatter, treefmtの設定をflake-parts流に移行
    - flake.lockをflake-partsベースの依存関係に更新

--- flake.nix
+++ flake.nix
@@ -3,14 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    haskellNix = {
-      url = "github:input-output-hk/haskell.nix";
-      inputs.nixpkgs-2511.follows = "nixpkgs";
     };
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
@@ -38,208 +37,175 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
+    inputs@{
+      flake-parts,
       treefmt-nix,
-      haskellNix,
       pyproject-nix,
       uv2nix,
       pyproject-build-systems,
       html-tidy-src,
       ...
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
-      system:
-      let
-        overlays = [
-          haskellNix.overlay
-          (final: prev: {
-            # 公式リリースがしばらくないのでGitHubの最新版を利用。
-            html-tidy = prev.html-tidy.overrideAttrs (_oldAttrs: {
-              src = html-tidy-src;
-            });
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-            # JavaScriptパッケージを管理
-            npmRoot = final.lib.fileset.toSource {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        let
+          # 公式リリースがしばらくないのでGitHubの最新版を利用。
+          html-tidy = pkgs.html-tidy.overrideAttrs (_oldAttrs: {
+            src = html-tidy-src;
+          });
+
+          # JavaScriptパッケージを管理
+          npmRoot = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.unions [
+              ./package.json
+              ./package-lock.json
+            ];
+          };
+          npmDeps = pkgs.importNpmLock {
+            inherit npmRoot;
+          };
+          nodeEnv = pkgs.buildNpmPackage {
+            pname = "www-ncaq-net";
+            version = "0.1.1.0";
+            src = ./.;
+            inherit npmDeps;
+            inherit (pkgs.importNpmLock) npmConfigHook;
+            dontNpmBuild = true;
+            # devDependenciesのsass, prettierなども含める
+            npmFlags = [ "--include=dev" ];
+          };
+          nodeEnvLint = pkgs.buildNpmPackage {
+            name = "www-ncaq-net-lint";
+            src = ./.;
+            inherit npmDeps;
+            inherit (pkgs.importNpmLock) npmConfigHook;
+            npmBuildScript = "lint";
+            dontNpmInstall = true;
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out
+              touch $out/lint-passed
+              runHook postInstall
+            '';
+          };
+
+          # uv2nixでPythonパッケージを管理
+          pythonWorkspace = uv2nix.lib.workspace.loadWorkspace {
+            workspaceRoot = pkgs.lib.fileset.toSource {
               root = ./.;
-              fileset = final.lib.fileset.unions [
-                ./package.json
-                ./package-lock.json
+              fileset = pkgs.lib.fileset.unions [
+                ./pyproject.toml
+                ./uv.lock
               ];
             };
-            npmDeps = prev.importNpmLock {
-              npmRoot = final.npmRoot;
-            };
-            nodeEnv = prev.buildNpmPackage {
-              pname = "www-ncaq-net";
-              version = "0.1.1.0";
-              src = ./.;
-              npmDeps = final.npmDeps;
-              inherit (prev.importNpmLock) npmConfigHook;
-              dontNpmBuild = true;
-              # devDependenciesのsass, prettierなども含める
-              npmFlags = [ "--include=dev" ];
-            };
-            nodeEnvLint = prev.buildNpmPackage {
-              name = "www-ncaq-net-lint";
-              src = ./.;
-              npmDeps = final.npmDeps;
-              inherit (prev.importNpmLock) npmConfigHook;
-              npmBuildScript = "lint";
-              dontNpmInstall = true;
-              installPhase = ''
-                runHook preInstall
-                mkdir -p $out
-                touch $out/lint-passed
-                runHook postInstall
-              '';
-            };
+          };
+          pythonOverlay = pythonWorkspace.mkPyprojectOverlay {
+            sourcePreference = "wheel";
+          };
+          pythonBase = pkgs.callPackage pyproject-nix.build.packages {
+            python = pkgs.python312;
+          };
+          pythonSet = pythonBase.overrideScope (
+            pkgs.lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              pythonOverlay
+            ]
+          );
+          pythonEnv = pythonSet.mkVirtualEnv "www-ncaq-net-python-env" pythonWorkspace.deps.default;
 
-            # uv2nixでPythonパッケージを管理
-            pythonWorkspace = uv2nix.lib.workspace.loadWorkspace {
-              workspaceRoot = final.lib.fileset.toSource {
-                root = ./.;
-                fileset = final.lib.fileset.unions [
-                  ./pyproject.toml
-                  ./uv.lock
-                ];
+          # Haskellビルド
+          www-ncaq-net-unwrapped =
+            pkgs.haskell.lib.overrideCabal (pkgs.haskellPackages.callCabal2nix "www-ncaq-net" ./. { })
+              {
+                # cabal buildでzlibのC依存を解決するために必要。
+                executablePkgconfigDepends = with pkgs; [ zlib ];
               };
-            };
-            pythonOverlay = final.pythonWorkspace.mkPyprojectOverlay {
-              sourcePreference = "wheel";
-            };
-            pythonBase = prev.callPackage pyproject-nix.build.packages {
-              python = final.python312;
-            };
-            pythonSet = final.pythonBase.overrideScope (
-              prev.lib.composeManyExtensions [
-                pyproject-build-systems.overlays.default
-                final.pythonOverlay
-              ]
-            );
-            pythonEnv = final.pythonSet.mkVirtualEnv "www-ncaq-net-python-env" final.pythonWorkspace.deps.default;
+          www-ncaq-net = www-ncaq-net-unwrapped.overrideAttrs (oldAttrs: {
+            nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+            # Hakyllは実行時にsass, html-tidy等の外部コマンドを呼び出すため、
+            # makeWrapperでPATHに追加する。
+            postInstall = (oldAttrs.postInstall or "") + ''
+              wrapProgram $out/bin/www-ncaq-net \
+                --prefix PATH : ${
+                  pkgs.lib.makeBinPath [
+                    html-tidy
+                    nodeEnv
+                    pythonEnv
+                  ]
+                } \
+                --set NODE_PATH ${nodeEnv}/lib/node_modules/www-ncaq-net/node_modules
+            '';
+          });
+        in
+        {
+          packages = {
+            default = www-ncaq-net;
+          };
 
-            project = final.haskell-nix.stackProject' {
-              src = final.haskell-nix.haskellLib.cleanSourceWith {
-                src = ./.;
-                name = "www-ncaq-net-source";
+          # テストがないパッケージもビルドしてエラーを検出する。
+          checks = {
+            inherit www-ncaq-net nodeEnvLint;
+          };
+
+          treefmt = {
+            # actionlintはセルフホストランナーの設定ファイルを正常に読み込まなかった。
+            # yamlfmtはprettierと競合する。
+            projectRootFile = "flake.nix";
+            programs = {
+              cabal-gild.enable = true;
+              deadnix.enable = true;
+              fourmolu.enable = true;
+              hlint.enable = true;
+              nixfmt.enable = true;
+              shellcheck.enable = true;
+              shfmt.enable = true;
+              statix.enable = true;
+
+              prettier = {
+                enable = true;
+                excludes = [ "*.md" ];
               };
-              name = "www-ncaq-net";
-              modules = [
-                {
-                  packages.www-ncaq-net.components.exes.www-ncaq-net = {
-                    # Hakyllは実行時にsass, html-tidy等の外部コマンドを呼び出すため、
-                    # makeWrapperでPATHに追加する。
-                    postInstall = ''
-                      wrapProgram $out/bin/www-ncaq-net \
-                        --prefix PATH : ${
-                          final.lib.makeBinPath [
-                            final.html-tidy
-                            final.nodeEnv
-                            final.pythonEnv
-                          ]
-                        } \
-                        --set NODE_PATH ${final.nodeEnv}/lib/node_modules/www-ncaq-net/node_modules
-                    '';
-                  };
-                }
-              ];
-              shell = {
-                tools = {
-                  fourmolu = "latest";
-                  haskell-language-server = "latest";
-                  hlint = "latest";
-                  stack = "latest";
-                };
-                buildInputs = with final; [
-                  # Haskell
-                  (writeScriptBin "haskell-language-server-wrapper" ''
-                    #!${stdenv.shell}
-                    exec haskell-language-server "$@"
-                  '')
-
-                  html-tidy
-                  nodeEnv
-                  nodejs
-                  pythonEnv
-                  uv
-                ];
-              };
-            };
-          })
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          inherit (haskellNix) config;
-        };
-        flake = pkgs.project.flake { };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs (_: {
-          # actionlintはセルフホストランナーの設定ファイルを正常に読み込まなかった。
-          # yamlfmtはprettierと競合する。
-          projectRootFile = "flake.nix";
-          programs = {
-            cabal-gild.enable = true;
-            deadnix.enable = true;
-            hlint.enable = true;
-            nixfmt.enable = true;
-            shellcheck.enable = true;
-            shfmt.enable = true;
-            statix.enable = true;
-
-            fourmolu = {
-              enable = true;
-              package = pkgs.fourmolu;
-            };
-            prettier = {
-              enable = true;
-              excludes = [ "*.md" ];
             };
           };
-        });
-      in
-      # hydraJobsもGitHub Actionsを使うため不要なので除外。
-      builtins.removeAttrs flake [
-        "ciJobs"
-        "hydraJobs"
-      ]
-      // {
-        packages = flake.packages // {
-          default = flake.packages."www-ncaq-net:exe:www-ncaq-net";
-        };
-        formatter = treefmtEval.config.build.wrapper;
-        checks =
-          flake.packages # テストがないパッケージもビルドしてエラーを検出する。
-          // flake.checks
-          // {
-            inherit (pkgs) nodeEnvLint;
-            formatting = treefmtEval.config.build.check self;
-          };
-        devShells.default =
-          flake.devShells.default
-          // pkgs.mkShell {
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ www-ncaq-net-unwrapped ];
             packages = with pkgs; [
+              cabal-install
+              fourmolu
+              haskell-language-server
+              hlint
+
+              html-tidy
+
               importNpmLock.hooks.linkNodeModulesHook
+              nodeEnv
+              nodejs
+
               pythonEnv
               uv
             ];
             npmDeps = pkgs.importNpmLock.buildNodeModules {
               inherit (pkgs) nodejs;
-              npmRoot = pkgs.npmRoot;
+              inherit npmRoot;
             };
           };
-      }
-    );
+        };
+    };
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.nixos.org"
-      "https://cache.iog.io"
       "https://www-ncaq-net.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "www-ncaq-net.cachix.org-1:muU00ItcyCf+F+lS//4w9XxW+UZYQxGa1cra1VXRH8c="
     ];
     allow-import-from-derivation = true;
