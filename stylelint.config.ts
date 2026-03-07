9b49d810665cf2ea56efa13277140d1c0f978a13
ncaq <ncaq@ncaq.net>
2026-03-07 15:45:01 +0900

    build(deps-dev): Stylelintと関連プラグインを導入して設定を追加
    これの準備。#271
    まだstylelintを導入しただけでlintする対象のファイルは作っていません。
    scssは将来的には消す予定なので、
    lintの対象にはしていませんし、
    そもそも対象を設定していません。
    目的としてはweb標準と無意味なルールの防止とアクセシビリティをある程度チェックするだけのものです。

---
+++ stylelint.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "stylelint";
+
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-clean-order", "@double-great/stylelint-a11y/strict"],
+  plugins: ["stylelint-browser-compat", "stylelint-declaration-block-no-ignored-properties"],
+  rules: {
+    "plugin/browser-compat": true,
+    "plugin/declaration-block-no-ignored-properties": true,
+  },
+} satisfies Config;
