e45d521f08f87a5ba1917a8cc3b16f4c423e1e9b
ncaq <ncaq@ncaq.net>
2026-03-07 15:55:26 +0900

    build(stylelint): ビルド生成物をignoreFilesに追加

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "stylelint";
 
 export default {
+  ignoreFiles: ["result/**/*", "_cache/**/*", "_site/**/*"],
   extends: ["stylelint-config-standard", "stylelint-config-clean-order", "@double-great/stylelint-a11y/strict"],
   plugins: ["stylelint-browser-compat", "stylelint-declaration-block-no-ignored-properties"],
   rules: {
