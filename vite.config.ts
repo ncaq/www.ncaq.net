9a7e39ec78d314039bba0a742b5a32f59aaf4a46
ncaq <ncaq@ncaq.net>
2026-04-30 12:01:28 +0900

    feat: viteによるCSSバンドルの追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    rolldownOptions: {
+      input: "site/style/main.css",
+      output: {
+        assetFileNames: "dist/css/bundle.css",
+      },
+    },
     cssCodeSplit: false,
-  },
-  css: {
-    devSourcemap: true,
   },
 });
