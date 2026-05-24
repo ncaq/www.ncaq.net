30fd2ed734ff8f86e7829d3e20f4ecba0b753751
ncaq <ncaq@ncaq.net>
2026-05-24 13:03:15 +0900

    style: `nix fmt`による自動整形

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -2,7 +2,11 @@ import type { Config } from "stylelint";
 
 export default {
   ignoreFiles: ["result/**/*", "_cache/**/*", "_site/**/*"],
-  extends: ["stylelint-config-standard", "stylelint-config-clean-order", "@double-great/stylelint-a11y/strict"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-clean-order",
+    "@double-great/stylelint-a11y/strict",
+  ],
   plugins: ["stylelint-browser-compat", "stylelint-declaration-block-no-ignored-properties"],
   rules: {
     "plugin/browser-compat": true,
